5f1c9c326f24469cf911711c89f24abe898a75ee
Potiphar10 <namgapotifar10@gmail.com>
2024-08-17 13:19:40 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed= new Typed(".text", {
+    strings:["Fronted developer" , "Graphics Designer" , "2D Animator" , "Videographer" , "Script Writer" , "Web Designer" , "Cinematographer" , "Producer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
